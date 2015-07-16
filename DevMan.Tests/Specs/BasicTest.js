4c32f52d911d4bc7b6f8950f74145b842216bfcf
jestermax <jestermax@gmail.com>
2015-07-16 11:42:14 -0300

    First Checking
    Uploading all files

---
+++ DevMan.Tests/Specs/BasicTest.js
@@ -0,0 +1,18 @@
+﻿
+
+/**
+	Example UI unit test
+*/
+describe("Basic Test", function () {
+   
+    //beforeEach(module('Interface'));
+
+   
+    /**
+		This test checks a case that could fail other tests without explanation
+	*/
+    it("should display the correct status", function () {
+        expect(Add(1, 2)).toEqual(3);
+    });
+
+});
---
+++ DevMan/Scripts/Test.js
@@ -0,0 +1,4 @@
+﻿
+function Add(one, two) {
+    return one + two;
+}
